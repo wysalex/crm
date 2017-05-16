4328ad8fed33fe4d882384f4933568763f4e657d
allen <allen@sharetech.com.tw>
2017-05-16 18:12:12 +0800

    first commit

---
+++ src/components/Hey.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hey">
+    <h5>{{ msg }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'hey',
+
+  data () {
+    return {
+      msg: "Hey it's me"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Hey from '@/components/Hey'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    }, {
+      path: '/hey',
+      name: 'Hey',
+      component: Hey
+    }
+  ]
+})
