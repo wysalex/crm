459b4e34e13ad40e6a488a81da1ad026a01c2b05
wysalex <wysalexwang@gmail.com>
2017-05-21 21:25:53 +0800

    新增 firebase

--- src/main.js
+++ src/main.js
@@ -4,9 +4,49 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMdl from 'vue-mdl'
+import firebase from 'firebase'
+import VueFire from 'vuefire'
 
 Vue.config.productionTip = false
 
+Vue.use(VueFire)
+Vue.use(VueMdl)
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyDyZlqHISZWYp_vsyoLtpBwCVHnJTm_J6k',
+  authDomain: 'my-cms-9c5f1.firebaseapp.com',
+  databaseURL: 'https://my-cms-9c5f1.firebaseio.com',
+  projectId: 'my-cms-9c5f1',
+  storageBucket: 'my-cms-9c5f1.appspot.com',
+  messagingSenderId: '61829099555'
+}
+const app = firebase.initializeApp(firebaseConfig)
+const email = 'wysalexwang@gmail.com'
+const password = 'PassWord'
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+  var errorCode = error.code
+  var errorMessage = error.message
+  console.log(errorCode)
+  console.log(errorMessage)
+})
+
+const db = firebase.database()
+
+// Globals variable
+const globals = {
+  router,
+  app,
+  firebase,
+  db
+}
+
+Vue.mixin({
+  created: function () {
+    this.global = globals
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
